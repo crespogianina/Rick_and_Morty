3475e3484e623c0bfcbd7d74bd9e95913e938387
crespogianina <crespogianina98@gmail.com>
2023-04-12 22:52:35 -0300

    little changes

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,20 @@
 import SearchBar from './SearchBar';
 import { NavLink } from 'react-router-dom';
+import style from './SearchBar.module.css'
 
 
 export default function Nav ({onSearch, searchRandom}) {
     return (
-        <div>
-            <SearchBar onSearch={onSearch} searchRandom={searchRandom}/>
-            <NavLink to='/about' ><button>About</button></NavLink>
-            <NavLink to='/home'><button >Home</button></NavLink>
-            <NavLink to='/favorites'><button>Favorites</button></NavLink>
-        </div>
+        <nav className={style.navConteiner}>
+            <div className={style.links}>
+            <NavLink to='/about' ><button className={style.button}>About</button></NavLink>
+            <NavLink to='/home'><button className={style.button} >Home</button></NavLink>
+            <NavLink to='/favorites'><button className={style.button} >Favorites</button></NavLink>
+            </div>
+            <div className={style.SearchBar}>
+            <SearchBar onSearch={onSearch} searchRandom={searchRandom} />  
+            </div>
+        
+        </nav>
     )
 }
